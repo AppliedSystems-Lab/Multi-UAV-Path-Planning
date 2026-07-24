e393eec32268cafca74b670b9ece295603370602
AliCS-dev <muhammadalikhanhitec@gmail.com>
2026-07-24 20:01:51 +0200

    meow

--- single_Uav_mission.m
+++ single_Uav_mission.m
@@ -1,15 +1,20 @@
 scene = Create_Scenario_Cuboid();
-waypoints = [309.281, 323.134, 200;
-             144.586, 290.189, 200;
-             156.379, 192.483, 200;
-             321.443, 213.848, 200;
-             309.281, 323.134, 200];
+waypoints = [-81.1517, 109.077, 300;
+             -69.8244, -79.9673, 250;
+             336.47, -184.503, 300;
+             327.181, 74.75, 300;
+             -81.1517, 109.077, 300];
 toa = [0 5 10 15 20];
 trajectory = waypointTrajectory(waypoints, TimeOfArrival=toa,SampleRate=1);
 uav = uavPlatform("Alpha1",scene,Trajectory=trajectory);
-updateMesh(uav,"quadrotor",{10},[1 0 0],eul2tform([0 0 pi]));
+updateMesh(uav,"quadrotor",{40},[1 0 0],eul2tform([0 0 pi]));
 
 fig_axis = show3D(scene);
 hold(fig_axis,"on");
 plot3(fig_axis,waypoints(:,1),waypoints(:,2),waypoints(:,3),"-o");
 hold(fig_axis,"off");
+setup(scene)
+while advance(scene)
+    show3D(scene,Parent=fig_axis,FastUpdate=true);
+    drawnow limitrate;
+end
